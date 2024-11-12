45ce8829d3217c49c4bbc79195dee054b9b79ef0
Virihj <87740256+Virihj@users.noreply.github.com>
2024-11-11 20:48:02 -0800

    added more media items Checkpoint2-updated-script.sql

--- Project/Checkpoint2-updated-script.sql
+++ Project/Checkpoint2-updated-script.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS MediaType;
 DROP TABLE IF EXISTS Platform;
 DROP TABLE IF EXISTS MediaTypeGenre;
-DROP TABLE IF EXISTS Author;
-DROP TABLE IF EXISTS AuthorItem;
 DROP TABLE IF EXISTS AgeRating;
 DROP TABLE IF EXISTS ItemAgeRating;
 
@@ -93,14 +91,22 @@ VALUES
     ('Fantasy', 2),      -- Books
     ('Sci-Fi', 2),       -- Books
     ('Rock', 3),         -- Music
-    ('Pop', 3);          -- Music
+    ('Pop', 3),          -- Music
+    ('Hip-Hop', 3);      -- Music
 
 -- Insert data into Item Table
 INSERT INTO Item (ItemName, ReleaseYear, GenreID)
 VALUES 
     ('God of War', 2018, 2),     -- RPG, Games
     ('Dune', 1965, 4),           -- Sci-Fi, Books
-    ('Crazy in Love', 2003, 6);  -- Pop, Music
+    ('Crazy in Love', 2003, 6),  -- Pop, Music
+    ('Fancy', 2010, 7),          -- Hip-Hop, Music
+    ('Uptown Funk', 2014, 6),    -- Pop, Music
+    ('Lose You to Love Me', 2019, 6), -- Pop, Music
+    ('Harry Potter and the Sorcerers Stone', 1997, 3),  -- Fantasy, Books
+    ('The Lightning Thief', 2005, 3),                   -- Fantasy, Books
+    ('Super Mario Odyssey', 2017, 1),                   -- Platformer, Games
+    ('Sonic the Hedgehog', 1991, 1);                    -- Platformer, Games
 
 -- Insert data into Platform Table
 INSERT INTO Platform (PlatformName)
@@ -108,7 +114,10 @@ VALUES
     ('PlayStation'),
     ('Steam'),
     ('Hardcover'),
-    ('Spotify');
+    ('Spotify'),
+    ('Nintendo Switch'),
+    ('Xbox'),
+    ('Apple Books');
 
 -- Insert data into AgeRating Table
 INSERT INTO AgeRating (AgeRatingName)
@@ -124,7 +133,14 @@ VALUES
     (1, 1),  -- God of War on PlayStation
     (1, 2),  -- God of War on Steam
     (2, 3),  -- Dune as Hardcover
-    (3, 4);  -- Crazy in Love on Spotify
+    (3, 4),  -- Crazy in Love on Spotify
+    (4, 4),  -- Fancy on Spotify
+    (5, 4),  -- Uptown Funk on Spotify
+    (6, 4),  -- Lose You to Love Me on Spotify
+    (7, 3),  -- Harry Potter on Hardcover
+    (8, 3),  -- The Lightning Thief on Hardcover
+    (9, 5),  -- Super Mario Odyssey on Nintendo Switch
+    (10, 5); -- Sonic the Hedgehog on Nintendo Switch
 
 -- Insert data into MediaTypeGenre Table
 INSERT INTO MediaTypeGenre (MediaTypeID, GenreID)
@@ -134,11 +150,19 @@ VALUES
     (2, 3),  -- Books linked with Fantasy genre
     (2, 4),  -- Books linked with Sci-Fi genre
     (3, 5),  -- Music linked with Rock genre
-    (3, 6);  -- Music linked with Pop genre
+    (3, 6),  -- Music linked with Pop genre
+    (3, 7);  -- Music linked with Hip-Hop genre
 
 -- Insert data into ItemAgeRating Table to link items to their age ratings
 INSERT INTO ItemAgeRating (ItemID, AgeRatingID)
 VALUES 
     (1, 2),  -- God of War has M for Mature rating
     (2, 3),  -- Dune has PG-13 rating
-    (3, 1);  -- Crazy in Love has E for Everyone rating
+    (3, 1),  -- Crazy in Love has E for Everyone rating
+    (4, 1),  -- Fancy has E for Everyone rating
+    (5, 1),  -- Uptown Funk has E for Everyone rating
+    (6, 1),  -- Lose You to Love Me has E for Everyone rating
+    (7, 1),  -- Harry Potter has E for Everyone rating
+    (8, 1),  -- The Lightning Thief has E for Everyone rating
+    (9, 1),  -- Super Mario Odyssey has E for Everyone rating
+    (10, 1); -- Sonic the Hedgehog has E for Everyone rating
