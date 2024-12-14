f6bf5ea3d8768f3ed3820d3823c7061c7025c52f
Virihj <87740256+Virihj@users.noreply.github.com>
2024-12-13 17:28:03 -0800

    Final_Updated_Script

--- Project/Checkpoint3-script.sql
+++ Project/Checkpoint3-script.sql
@@ -89,6 +89,15 @@ CREATE TABLE ItemAuthor (
     FOREIGN KEY (AuthorID) REFERENCES Author(AuthorID),
     UNIQUE (ItemID, AuthorID) -- Ensure each Item-Author combination is unique
 );
+--9 Create Junction Table for Item and Author
+CREATE TABLE ItemAgeRating (
+    ItemAgeRatingID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ItemID INTEGER NOT NULL,
+    AgeRatingID INTEGER NOT NULL,
+    FOREIGN KEY (ItemID) REFERENCES Item(ItemID),
+    FOREIGN KEY (AgeRatingID) REFERENCES AgeRating(AgeRatingID),
+    UNIQUE (ItemID, AgeRatingID) -- Ensure each Item-AgeRatingID combination is unique
+);
 
 --11 Insert data into MediaType Table, including ID 6 for "Mix"
 INSERT INTO MediaType (MediaTypeName)
@@ -150,69 +159,147 @@ VALUES
     (3, 'PG-13'),             -- 3
     (4, 'R');                 -- 4
 
--- 17 Insert data into Author Table
-INSERT INTO Author (AuthorName)
-VALUES
-    ('David Jaffe'),       -- 1 Creator of God of War
-    ('Frank Herbert'),     -- 2 Author of Dune
-    ('Beyoncé'),           -- 3 Artist for Crazy in Love
-    ('Iggy Azalea'),       -- 4 Artist for Fancy
-    ('Mark Ronson'),       -- 5 Artist for Uptown Funk
-    ('Selena Gomez'),      -- 6 Artist for Lose You to Love Me
-    ('Stan Lee'),          -- 7 Writer for Spider-Man: Homecoming
-    ('Frank Miller'),      -- 8 Writer for Batman: Year One
-    ('Chris Claremont'),   -- 9 Writer for X-Men: Days of Future Past
-    ( 'Geoff Johns'),      -- 10 Writer for Justice League: Origin
-    ('Leonardo da Vinci'),-- 11 Painter of The Last Supper
-    ('Johannes Vermeer'), -- 12 Painter of Girl with a Pearl Earring
-    ('Michelangelo');     -- 13 Painter of The Creation of Adam
-
-
-
 -- Insert more data into Item Table
 INSERT INTO Item (ItemName, ReleaseYear, GenreID, MediaTypeID, AuthorID, PlatformID, AgeRatingID)
 VALUES
-    ('Crazy in Love', 2003, 10, 3, 2, 3, 3),             -- Pop, AuthorID: Beyoncé, Music, T for Teen
-    ('Fancy', 2010, 11, 3, 2, 3, 3),                    -- Hip-Hop, AuthorID: Iggy Azalea, Music, T for Teen
-    ('Uptown Funk', 2014, 10, 3, 2, 3, 3),              -- Pop, AuthorID: Mark Ronson, Music, T for Teen
-    ('Lose You to Love Me', 2019, 10, 3, 2, 3, 3),      -- Pop, AuthorID: Selena Gomez, Music, T for Teen
-    ('Harry Potter and the Sorcerers Stone', 1997, 6, 2, 3, 1, 1), -- Fantasy, AuthorID: J.K. Rowling, Hardcover, E for Everyone
-    ('The Lightning Thief', 2005, 6, 2, 3, 1, 1),       -- Fantasy, AuthorID: Rick Riordan, Hardcover, E for Everyone
-    ('Super Mario Odyssey', 2017, 1, 1, 1, 2, 2),       -- FPS, AuthorID: Nintendo, Games, M for Mature
-    ('Sonic the Hedgehog', 1991, 1, 1, 1, 2, 2),        -- FPS, AuthorID: SEGA, Games, M for Mature
-    ('Spider-Man: Homecoming', 2017, 8, 4, 3, 2, 2),    -- Comic, AuthorID: Marvel, Movies/Shows, M for Mature
-    ('Batman: Year One', 1987, 8, 4, 3, 2, 2),          -- Comic, AuthorID: DC, Movies/Shows, M for Mature
-    ('X-Men: Days of Future Past', 1981, 8, 4, 3, 2, 2), -- Comic, AuthorID: Marvel, Movies/Shows, M for Mature
-    ('Justice League: Origin', 2011, 8, 4, 3, 2, 2),    -- Comic, AuthorID: DC, Movies/Shows, M for Mature
-    ('Mona Lisa', 1503, 19, 5, 4, 1, 1),                -- Renaissance, AuthorID: Leonardo da Vinci, Art, E for Everyone
-    ('The Last Supper', 1498, 19, 5, 4, 1, 1),          -- Renaissance, AuthorID: Leonardo da Vinci, Art, E for Everyone
-    ('Starry Night', 1889, 19, 5, 4, 1, 1),             -- Renaissance, AuthorID: Vincent van Gogh, Art, E for Everyone
-    ('Girl with a Pearl Earring', 1665, 19, 5, 4, 1, 1), -- Renaissance, AuthorID: Johannes Vermeer, Art, E for Everyone
-    ('The Creation of Adam', 1512, 19, 5, 4, 1, 1);     -- Renaissance, AuthorID: Michelangelo, Art, E for Everyone
+    ('The Legend of Zelda: Breath of the Wild', 2017, 2, 1, 1, 5, 1), -- RPG, Nintendo Switch, E for Everyone
+    ('God of War', 2018, 2, 1, 2, 1, 2),                            -- RPG, PlayStation, M for Mature
+    ('Red Dead Redemption 2', 2018, 2, 1, 3, 1, 2),                -- RPG, PlayStation, M for Mature
+    ('Fortnite', 2017, 5, 1, 4, 6, 1),                             -- MOBA, Xbox, E for Everyone
+    ('Cyberpunk 2077', 2020, 2, 1, 5, 2, 2),                       -- RPG, Steam, M for Mature
+    ('Elden Ring', 2022, 2, 1, 6, 2, 2),                           -- RPG, Steam, M for Mature
+    ('Among Us', 2018, 4, 1, 7, 2, 1),                             -- MMO, Steam, E for Everyone
+    ('Genshin Impact', 2020, 2, 1, 8, 8, 1),                       -- RPG, Digital, E for Everyone
+    ('Hollow Knight', 2017, 3, 1, 9, 2, 1),                        -- Story, Steam, E for Everyone
+    ('Minecraft Dungeons', 2020, 3, 1, 10, 6, 1),                  -- Story, Xbox, E for Everyone
+    ('Crazy in Love', 2003, 10, 3, 3, 4, 3),                       -- Pop, Beyoncé, Music, PG-13
+    ('Fancy', 2010, 11, 3, 4, 4, 3),                               -- Hip-Hop, Iggy Azalea, Music, PG-13
+    ('Uptown Funk', 2014, 10, 3, 5, 4, 3),                         -- Pop, Mark Ronson, Music, PG-13
+    ('Lose You to Love Me', 2019, 10, 3, 6, 4, 3),                 -- Pop, Selena Gomez, Music, PG-13
+    ('Harry Potter and the Sorcerers Stone', 1997, 6, 2, 11, 3, 1),-- Fantasy, J.K. Rowling, Hardcover, E for Everyone
+    ('The Lightning Thief', 2005, 6, 2, 11, 3, 1),                 -- Fantasy, Rick Riordan, Hardcover, E for Everyone
+    ('Spider-Man: Homecoming', 2017, 8, 4, 7, 1, 2),               -- Comic, Stan Lee, Movies/Shows, M for Mature
+    ('Batman: Year One', 1987, 8, 4, 8, 1, 2),                     -- Comic, Frank Miller, Movies/Shows, M for Mature
+    ('X-Men: Days of Future Past', 1981, 8, 4, 9, 1, 2),           -- Comic, Chris Claremont, Movies/Shows, M for Mature
+    ('Justice League: Origin', 2011, 8, 4, 10, 1, 2),              -- Comic, Geoff Johns, Movies/Shows, M for Mature
+    ('Mona Lisa', 1503, 19, 5, 11, 10, 1),                         -- Renaissance, Leonardo da Vinci, Art, E for Everyone
+    ('The Last Supper', 1498, 19, 5, 11, 10, 1),                   -- Renaissance, Leonardo da Vinci, Art, E for Everyone
+    ('Starry Night', 1889, 19, 5, 12, 10, 1),                      -- Renaissance, Vincent van Gogh, Art, E for Everyone
+    ('Girl with a Pearl Earring', 1665, 19, 5, 13, 10, 1),         -- Renaissance, Johannes Vermeer, Art, E for Everyone
+    ('The Creation of Adam', 1512, 19, 5, 14, 10, 1);              -- Renaissance, Michelangelo, Art, E for Everyone
 
+-- Insert more data into Author Table
+INSERT INTO Author (AuthorID, AuthorName)
+VALUES
+    (1, 'Nintendo'),
+    (2, 'Santa Monica Studio'),
+    (3, 'Rockstar Games'),
+    (4, 'Epic Games'),
+    (5, 'CD Projekt Red'),
+    (6, 'FromSoftware'),
+    (7, 'Innersloth'),
+    (8, 'miHoYo'),
+    (9, 'Team Cherry'),
+    (10, 'Mojang Studios'),
+    (11, 'J.K. Rowling'),
+    (12, 'Vincent van Gogh'),
+    (13, 'Johannes Vermeer'),
+    (14, 'Michelangelo'),
+    (15, 'Beyoncé'),
+    (16, 'Iggy Azalea'),
+    (17, 'Mark Ronson'),
+    (18, 'Selena Gomez'),
+    (19, 'Stan Lee'),
+    (20, 'Frank Miller'),
+    (21, 'Chris Claremont'),
+    (22, 'Geoff Johns');
 
--- --14 Insert data into Item Table
--- INSERT INTO Item (ItemName, ReleaseYear, GenreID)
--- VALUES
---     ('God of War', 2018, 2),                           -- RPG, Games
---     ('Dune', 1965, 7),                                 -- Sci-Fi, Books
---     ('Crazy in Love', 2003, 10),                       -- Pop, Music
---     ('Fancy', 2010, 11),                               -- Hip-Hop, Music
---     ('Uptown Funk', 2014, 10),                         -- Pop, Music
---     ('Lose You to Love Me', 2019, 10),                 -- Pop, Music
---     ('Harry Potter and the Sorcerers Stone', 1997, 6), -- Fantasy, Books
---     ('The Lightning Thief', 2005, 6),                  -- Fantasy, Books
---     ('Super Mario Odyssey', 2017, 1),                  -- FPS, Games
---     ('Sonic the Hedgehog', 1991, 1),                   -- FPS, Games
---     ('Spider-Man: Homecoming', 2017, 8),               -- Comic, Books
---     ('Batman: Year One', 1987, 8),                     -- Comic, Books
---     ('X-Men: Days of Future Past', 1981, 8),           -- Comic, Books
---     ('Justice League: Origin', 2011, 8),               -- Comic, Books
---     ('Mona Lisa', 1503, 19),                           -- Renaissance, Art
---     ('The Last Supper', 1498, 19),                     -- Renaissance, Art
---     ('Starry Night', 1889, 19),                        -- Renaissance, Art
---     ('Girl with a Pearl Earring', 1665, 19),           -- Renaissance, Art
---     ('The Creation of Adam', 1512, 19);                -- Renaissance, Art
+-- Insert data into ItemAuthor Table to link items with authors
+INSERT INTO ItemAuthor (ItemID, AuthorID)
+VALUES
+    (1, 2), -- The Legend of Zelda: Breath of the Wild by Nintendo
+    (2, 4), -- God of War by Santa Monica Studio
+    (3, 3), -- Red Dead Redemption 2 by Rockstar Games
+    (4, 1), -- Fortnite by Epic Games
+    (5, 2), -- Cyberpunk 2077 by CD Projekt Red
+    (6, 2), -- Elden Ring by FromSoftware
+    (7, 1), -- Among Us by Innersloth
+    (8, 2), -- Genshin Impact by miHoYo
+    (9, 2), -- Hollow Knight by Team Cherry
+    (10, 2), -- Minecraft Dungeons by Mojang Studios
+    (11, 1), -- Crazy in Love by Beyoncé
+    (12, 1), -- Fancy by Iggy Azalea
+    (13, 1), -- Uptown Funk by Mark Ronson
+    (14, 1), -- Lose You to Love Me by Selena Gomez
+    (15, 1), -- Harry Potter and the Sorcerers Stone by J.K. Rowling
+    (16, 1), -- The Lightning Thief by Rick Riordan
+    (17, 2), -- Spider-Man: Homecoming by Stan Lee
+    (18, 2), -- Batman: Year One by Frank Miller
+    (19, 2), -- X-Men: Days of Future Past by Chris Claremont
+    (20, 2), -- Justice League: Origin by Geoff Johns
+    (21, 1),
+    (22, 1),
+    (23, 1),
+    (24, 1),
+    (25, 1);
+
+-- Insert additional data into ItemPlatform Table
+INSERT OR IGNORE INTO ItemPlatform (ItemID, PlatformID)
+VALUES
+    (1, 5),
+    (2, 1),
+    (3, 1),
+    (4, 6),
+    (5, 2),
+    (6, 2),
+    (7, 2),
+    (8, 8),
+    (9, 2),
+    (10, 6),
+    (11, 4),
+    (12, 4),
+    (13, 4),
+    (14, 4),
+    (15, 3),
+    (16, 3),
+    (17, 1),
+    (18, 1),
+    (19, 1),
+    (20, 1),
+    (21, 10),
+    (22, 10),
+    (23, 10),
+    (24, 10),
+    (25, 10);
+
+-- Insert data into ItemAuthor Table to link items with authors
+INSERT INTO ItemAgeRating (ItemID, AgeRatingID)
+VALUES
+    (1, 1), -- The Legend of Zelda: Breath of the Wild by Nintendo
+    (2, 2), -- God of War by Santa Monica Studio
+    (3, 3), -- Red Dead Redemption 2 by Rockstar Games
+    (4, 4), -- Fortnite by Epic Games
+    (5, 5), -- Cyberpunk 2077 by CD Projekt Red
+    (6, 6), -- Elden Ring by FromSoftware
+    (7, 7), -- Among Us by Innersloth
+    (8, 8), -- Genshin Impact by miHoYo
+    (9, 9), -- Hollow Knight by Team Cherry
+    (10, 10), -- Minecraft Dungeons by Mojang Studios
+    (11, 15), -- Crazy in Love by Beyoncé
+    (12, 16), -- Fancy by Iggy Azalea
+    (13, 17), -- Uptown Funk by Mark Ronson
+    (14, 18), -- Lose You to Love Me by Selena Gomez
+    (15, 11), -- Harry Potter and the Sorcerers Stone by J.K. Rowling
+    (16, 11), -- The Lightning Thief by Rick Riordan
+    (17, 19), -- Spider-Man: Homecoming by Stan Lee
+    (18, 20), -- Batman: Year One by Frank Miller
+    (19, 21), -- X-Men: Days of Future Past by Chris Claremont
+    (20, 22), -- Justice League: Origin by Geoff Johns
+    (21, 11),
+    (22, 11),
+    (23, 12),
+    (24, 13),
+    (25, 14);
 
 
 --1 updated query for searching items by authors
